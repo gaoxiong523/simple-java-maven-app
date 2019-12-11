6123437f9b043af2aa8479341e6b56097f2e9fa4
gaoxiong523 <200506725@qq.com>
2019-12-11 15:43:12 +0800

    110

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -40,6 +40,11 @@ public class AppTest
         }
     }
 
+    @Test
+    public void myTest(){
+        System.out.println("AppTest.myTest + this is jenkins test");
+    }
+
     @After
     public void cleanUpStreams() {
         System.setOut(null);
